39e1dcebdf338cdfef2874b8cdfd2ce8c1f5a327
Tanmesh <tanmeshnm@Tanmeshs-MBP.attlocal.net>
2023-07-06 16:08:50 -0700

    adding executor_service

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,16 +1,31 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
 public class App {
   public static void main(String[] args) {
     String filePath = "data/tmp.txt";
     KVStore _kvStore = new KVStore(filePath);
 
-    _kvStore.put("1", "1");
-    _kvStore.put("2", "1");
-    _kvStore.put("3", "1");
-    _kvStore.put("4", "1");
-    _kvStore.put("5", "1");
+    int nThreads = 10;
+    ExecutorService executorService = Executors.newFixedThreadPool(nThreads);
 
-    System.out.println(_kvStore.get("1"));
-    System.out.println(_kvStore.get("3"));
-    System.out.println(_kvStore.get("2"));
+    // PUT
+    for (int i = 0; i < nThreads; ++i) {
+      final int index = i;
+      executorService.execute(() -> {
+        String key = "key" + index;
+        String value = "value" + index;
+        _kvStore.put(key, value);
+      });
+    }
+
+    executorService.shutdown();
+    while (!executorService.isTerminated()) ;
+
+    // GET
+    for (int i = 0; i < nThreads; ++i) {
+      System.out.println("Key" + i + " - " + _kvStore.get("key" + i));
+    }
   }
 }
